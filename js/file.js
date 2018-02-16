e77906d4d76f03aa1af81af2cfa3947e5e705abe
GERVIER <r.gervier@orange.fr>
2018-02-16 20:36:06 +0100

    merged with clement part

--- js/file.js
+++ js/file.js
@@ -1,4 +1,10 @@
 var objectUrl;
+var switchViewButton = document.getElementById('switchViewButton');
+
+var letStartButton = document.getElementById('start_button');
+var bool =1;
+
+var file = document.getElementById('filetype');
 
 $("#audio").on("canplaythrough", function(e){
     var seconds = e.currentTarget.duration;
@@ -27,12 +33,18 @@ $("#file").change(function(e){
 });
 
 $('#start_button').on('click', function(e){
-    e.preventDefault();
-    if ($(this).hasClass('btn-danger')) {
-            $(this).text('Let\'s start').removeClass('btn-danger').addClass("btn-success");
-    } else {
-        $(this).text('stop').addClass('btn-danger').removeClass("btn-success");
+
+    if(!file.innerHTML==""){
+        e.preventDefault();
+        if ($(this).hasClass('btn-danger')) {
+            $(this).text(' Let\'s start ').removeClass('btn-danger').addClass("btn-success");
+        } else {
+            $(this).text('stop').addClass('btn-danger').removeClass("btn-success");
+        }
+    }else {
+       $('#alertNoFile').modal('show');
     }
+    
 });
 
 $('.stop_button').on('click', function(e) {
@@ -45,9 +57,37 @@ function snackbarFunction() {
     // Get the snackbar DIV
     var x = document.getElementById("snackbar")
 
-    // Add the "show" class to DIV
-    x.className = "show";
+    if(letStartButton.innerHTML == " Let's start " && file.innerHTML!=""){
+        // Add the "show" class to DIV
+        x.className = "show";
+        // TODO Adapter le temps de la snackbar au temp de communication avec le serveur !!! remove the show class from DIV
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+    }
+}
 
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+$("#card").flip({
+    axis: 'y',
+    trigger: 'click',
+forceWidth: false,
+forceHeight: false
+  }).find('.front, .back').css({
+'width': '100%',
+'height': '100%'
+});
+
+$("#switchViewButton").click(function(){
+    $("#card").flip('toggle');
+  });
+  
+function changeText() {
+    if(bool=== 0)
+    {
+        switchViewButton.innerHTML = "Graph view <i class=\"fas fa-sync-alt\" aria-hidden=\"true\">";
+        bool=1;
+    }
+    else
+    {
+        switchViewButton.innerHTML = "Carousel view <i class=\"fas fa-sync-alt\" aria-hidden=\"true\">";
+        bool = 0;
+    }
 }
